0f6312d3cb1312061866c16f868cea6929a3719d
P1ton <Piton223@yandex.ru>
2024-08-20 00:51:51 +0900

    Add youtube routes

--- update-routes/chklist.sh
+++ update-routes/chklist.sh
@@ -1,6 +1,7 @@
 cd /opt/lists
 
 wget --no-if-modified-since -N https://antifilter.download/list/ipsum.lst https://antifilter.download/list/subnet.lst
+curl https://www.gstatic.com/ipranges/goog.json | jq -r '.prefixes[].ipv4Prefix | select( . != null)' > youtube.lst
 if [ -e ./md5.txt ]
 then
   old=$(cat ./md5.txt);
@@ -14,6 +15,7 @@ if [ "$old" != "$new" ]
 then
   cat ./ipsum.lst | sed 's_.*_route & reject;_' > ./ipsum.txt
   cat ./subnet.lst | sed 's_.*_route & reject;_' > ./subnet.txt
+  cat ./youtube.lst | sed 's_.*_route & reject;_' > ./youtube.txt
   /usr/sbin/birdc -s /var/run/bird/bird.ctl configure;
   logger "RKN list reconfigured";
   echo $new >./md5.txt;
